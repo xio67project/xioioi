b90b94d7d0b25fb954f4f8004a5f1c97c2bc9e37
sioworker <leon@winkids.pl>
2026-09-25 01:35:57 +0200

    switch out javascript for typescript

--- src/frontend/web/ts/ping.ts
+++ src/frontend/web/ts/ping.ts
@@ -1,11 +1,16 @@
-const api = "/api/ping";
-const out = document.getElementById("out");
+type Pong = {
+	pong: boolean;
+	t: number;
+};
 
-async function ping() {
+const api = "/api/ping";
+const out = document.getElementById("out") as HTMLElement;
+
+async function ping(): Promise<void> {
 	const start = performance.now();
 	try {
 		const res = await fetch(api);
-		const json = await res.json();
+		const json: Pong = await res.json();
 		const ms = (performance.now() - start).toFixed(1);
 		out.textContent = `${res.status} ${JSON.stringify(json)} ${ms}ms`;
 	} catch (e) {
